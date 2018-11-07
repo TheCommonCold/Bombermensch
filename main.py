9e907fd50b237e7f56ee7f7c403644306aee4a49
Luker <luker.plus@gmail.com>
2018-11-07 22:45:24 +0100

    1

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ import pygame
 import socket
 
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
 done = False
 is_blue = True
 x = 30
 y = 30
+map_size=[13,13]
+square_size=40
+screen = pygame.display.set_mode((map_size[0] * square_size, map_size[1] * square_size))
+map=[[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+     [1, 0, 1, 2, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
 def przeslanko():
     UDP_IP_ADDRESS = "192.168.0.50"
@@ -18,26 +33,34 @@ def przeslanko():
 
 clock = pygame.time.Clock()
 
+def draw_map():
+    i=0
+    for array in map:
+        j=0
+        for x in array:
+            if(x==1):
+                pygame.draw.rect(screen, (50, 50, 50), pygame.Rect(i*square_size, j*square_size, square_size, square_size))
+            if(x==2):
+                pygame.draw.rect(screen, (75, 75, 75),pygame.Rect(i * square_size, j * square_size, square_size, square_size))
 
+            j=j+1
+        i=i+1
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            is_blue = not is_blue
 
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP]: y -= 3
-    if pressed[pygame.K_DOWN]: y += 3
-    if pressed[pygame.K_LEFT]: x -= 3
-    if pressed[pygame.K_RIGHT]: x += 3
+    if pressed[pygame.K_UP]: y -= 2
+    if pressed[pygame.K_DOWN]: y += 2
+    if pressed[pygame.K_LEFT]: x -= 2
+    if pressed[pygame.K_RIGHT]: x += 2
 
-    screen.fill((0, 0, 0))
-    if is_blue:
-        color = (0, 128, 255)
-    else:
-        color = (255, 100, 0)
-    pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
+    screen.fill((100, 100, 100))
+    color = (0, 50, 150)
+    pygame.draw.circle(screen, color,(x,y),int((square_size/2)*0.8),0)
+
+    draw_map()
 
     pygame.display.flip()
     clock.tick(60)
